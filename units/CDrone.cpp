5f52b40f8e7ac7708057690ff6629bbf8f98b077
Thomas <Arcademiker@users.noreply.github.com>
2018-08-03 02:15:17 +0200

    Update CDrone.cpp
    bugfix

--- units/CDrone.cpp
+++ units/CDrone.cpp
@@ -52,7 +52,9 @@ bool CDrone::do_attack(GLFWwindow* window) {
     if(target4_id != 0 && this->map->get_unit(target4_id)->get_type() > 3) {
         targets.insert(std::make_pair(this->map->get_unit(target4_id)->get_type(),target4_id));
     }
-    this->attack(targets.begin()->second);
+    if(!target.empty()) {
+        this->attack(targets.begin()->second);
+    }
     targets.clear();
 
     return true;
